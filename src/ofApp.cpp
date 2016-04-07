97195e56b9165dbd6b03d4c1aa87115cc19b9642
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-04-07 10:31:58 +0200

    :tada: empty OP

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,53 +5,17 @@ void ofApp::setup(){
     /// Graphisme init  ///
     ofBackground(255, 255, 255);
     ofSetCircleResolution(60);
-    
-    //setup the server to listen on 11999
-    TCP.setup(11999);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    //for each client lets send them a message letting them know what port they are connected on
-//    for(int i = 0; i < TCP.getNumClients(); i++){
-//        TCP.send(i, "hello client - you are connected on port - "+ofToString(TCP.getClientPort(i)) );
-//    }
-    
-    //for each connected client lets get the data being sent and lets print it to the screen
-    for(int i = 0; i < TCP.getNumClients(); i++){
-        
-        //get the ip and port of the client
-        string port = ofToString( TCP.getClientPort(i) );
-        string ip   = TCP.getClientIP(i);
-        string info = "client "+ofToString(i)+" -connected from "+ip+" on port: "+port;
-        cout << info << endl;
-        
-        //we only want to update the text we have recieved there is data
-        string str = TCP.receive(i);
-        cout << str << endl;
-        
-    }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    //for each connected client lets get the data being sent and lets print it to the screen
-//    for(int i = 0; i < TCP.getNumClients(); i++){
-//        
-//        //get the ip and port of the client
-//        string port = ofToString( TCP.getClientPort(i) );
-//        string ip   = TCP.getClientIP(i);
-//        string info = "client "+ofToString(i)+" -connected from "+ip+" on port: "+port;
-//        cout << info << endl;
-//        
-//        //we only want to update the text we have recieved there is data
-//        string str = TCP.receive(i);
-//        cout << str << endl;
-//        
-//    }
 }
 
 
