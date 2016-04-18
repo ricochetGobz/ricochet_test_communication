46f6e7a09faf936c1c458f848eb759eab6ab4560
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-04-18 22:55:43 +0200

    fix placement of server.js file

--- server/app/server.js
+++ server/app/server.js
@@ -1,50 +1,46 @@
-var osc = require('node-osc');
+import osc from 'node-osc';
 
-let pokemon = "klk";
+const SENDER_PORT = 5555;
+const RECEIVER_PORT = 4444;
 
-var SENDER_PORT = 5555;
-var RECEIVER_PORT = 4444;
-
-var OPEN_FRAMEWORKS_CONNECTED = "/OPConnected";
-var PLAY = "/play";
+const OPEN_FRAMEWORKS_CONNECTED = '/OPConnected';
+const PLAY = '/play';
 
 
 // SEND MESSAGE
-var client = new osc.Client('127.0.0.1', SENDER_PORT);
-client.send('/newCubeConnected', "c1", function () {
+const client = new osc.Client('127.0.0.1', SENDER_PORT);
+client.send('/newCubeConnected', 'c1', () => {
   client.kill();
 });
 
-
 // RECEIVE MESSAGE
-var oscServer = new osc.Server(RECEIVER_PORT, '0.0.0.0');
+const oscServer = new osc.Server(RECEIVER_PORT, '0.0.0.0');
 
-oscServer.on("message", function (msg, rinfo) {
-
+oscServer.on('message', (message, rinfo) => {
+  let msg = message;
   // CHECK THE DATA STRUCTURE OF OSC MESSAGE
   // Open Framework don't send the same array
-  if(msg[0] === '#bundle') {
+  if (msg[0] === '#bundle') {
     msg = msg[2];
   }
 
-  var url = msg[0];
-  var message = msg[1];
+  const url = msg[0];
+  const content = msg[1];
 
-  console.log("New Message to :", url);
-  console.log("");
+  console.log('New Message to :', url);
 
   switch (url) {
     case OPEN_FRAMEWORKS_CONNECTED:
-      console.log("OpenFrameworks is connected !");
+      console.log('OpenFrameworks is connected !');
       break;
     case PLAY:
-      console.log("OpenFramework wants to active the cube "+message);
+      console.log(`OpenFramework wants to active the cube ${content}`);
       break;
     default:
-      console.warn("##### WARN");
-      console.warn("The "+url+" URL was not found :");
+      console.warn('##### WARN');
+      console.warn(`The ${url} URL was not found :`);
       console.warn(msg, rinfo);
-      console.warn("#####")
+      console.warn('#####');
       break;
   }
 });
